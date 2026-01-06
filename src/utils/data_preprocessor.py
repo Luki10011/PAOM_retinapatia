8e382705044b85b52d8ff760bea5895502b11369
Łukasz Nowosad <nowosad@student.agh.edu.pl>
2026-01-06 15:41:29 +0100

    .gitignore for python

---
+++ src/utils/data_preprocessor.py
@@ -0,0 +1,13 @@
+
+
+
+class RDPreprocessor:
+    def __init__(self,
+                 dataset_url,
+                 output_root, 
+                 ):
+        pass
+
+    def preprocess(self, data):
+        # Implement preprocessing logic here
+        pass
